758ecf9b29c62d463736e61e577dbe5187b958c1
Elaine D. Palacpac <elained.palacpac@Elaines-MacBook-Pro.local>
2025-12-29 17:01:15 +0800

    Fix redirect to /queueing-info for correct routing

--- resources/views/First.blade.php
+++ resources/views/First.blade.php
@@ -3,16 +3,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="0; url={{ url('/queueing_info') }}" />
+    <meta http-equiv="refresh" content="0; url={{ url('/queueing-info') }}" />
     <title>Redirecting...</title>
 </head>
 <body>
     <script>
-        window.location.href = "{{ url('/queueing_info') }}";
+        window.location.href = "{{ url('/queueing-info') }}";
     </script>
     <noscript>
-        <meta http-equiv="refresh" content="0; url={{ url('/queueing_info') }}" />
-        <p>If you are not redirected, <a href="{{ url('/queueing_info') }}">click here</a>.</p>
+        <meta http-equiv="refresh" content="0; url={{ url('/queueing-info') }}" />
+        <p>If you are not redirected, <a href="{{ url('/queueing-info') }}">click here</a>.</p>
     </noscript>
 </body>
 </html>
